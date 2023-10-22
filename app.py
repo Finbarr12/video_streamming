24c2a1488d82a254f395e7ae229069aceb430ef8
finbarr12 <ogbonnafinbarr@gmail.com>
2023-10-22 19:33:32 +0100

    added

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
+import cv2
 
 app = Flask(__name__)
+camera = cv2.VideoCapture(0)
 
 @app.route("/")
 def home():
-    return "Hello peoplwwe"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True) 
