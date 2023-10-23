2b21a12ad06741c099a99e25286498677d98244f
finbarr12 <ogbonnafinbarr@gmail.com>
2023-10-23 14:30:20 +0100

    finished

--- app.py
+++ app.py
@@ -7,17 +7,14 @@ camera = cv2.VideoCapture(0)
 
 def generate_frames():
     while True:
-
-        # read the camera frame
         success, frame = camera.read()
         if not success:
             break
-    else:
-        ret, buffer = cv2.imencode(".jpg", frame)
-        frame = buffer.tobytes()
-
-    yield (b'--frame\r\n'
-           b'Content-Type:image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            ret, buffer = cv2.imencode(".jpg", frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route("/")
@@ -27,7 +24,7 @@ def home():
 
 @app.route("/video")
 def video():
-    return Response(generate_frames, mimetype='multipart/x-mixed-replace;boundary=frame')
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace;boundary=frame')
 
 
 if __name__ == "__main__":
