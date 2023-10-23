ae6dfab37971c183783988acc81751be9362c388
finbarr12 <ogbonnafinbarr@gmail.com>
2023-10-23 13:41:17 +0100

    new

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask,render_template,Response
+from flask import Flask, render_template, Response
 import cv2
 
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
 
+
+def generate_frames():
+    while True:
+
+        success, frame = camera.read()
+        if not success:
+            break
+    else:
+        ret, buffer = cv2.imdecode('.jpg', frame)
+        frame = buffer.tobytes()
+
+        yield (b'')
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/video")
 def video():
-    return Response(func(),mimetype='multipart/x-mixed-replace;boundary=frame')
+    return Response(generate_frames, mimetype='multipart/x-mixed-replace;boundary=frame')
+
 
 if __name__ == "__main__":
-   app.run(debug=True) 
+    app.run(debug=True)
